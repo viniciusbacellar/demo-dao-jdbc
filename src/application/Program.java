f5de9b34e2eb39566ad6537dfece328af9615730
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-03-02 19:34:27 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,5 +25,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("==== TEST 3: seller FindAll ====");
+		
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
